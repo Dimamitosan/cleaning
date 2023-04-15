9aeed573f21327b9e699b96727a6ea7f5d3a9a51
Dmitrii Shagarov <shagarov07@gmail.com>
2023-04-16 00:18:52 +0300

    window_problems

--- src/script.js
+++ src/script.js
@@ -1,13 +1,3 @@
-import IMask from 'imask';
-
-var element = document.getElementById('phone');
-var maskOptions = {
-    mask: "+358-0-000-000",
-    lazy: false
-}
-
-var mask = new IMask(element, maskOptions)
-
 const modal = document.getElementById("my_modal");
 const btn = document.getElementById("calendar");
 const X = document.getElementsByClassName("btn-close")[0];
@@ -21,62 +11,21 @@ X.onclick = function(){
 }
 
 
-
-
-1
-2
-3
-4
-5
-6
-7
-
-	
-
-$('.art-stranger').mask('+3 8(099) 999-99-99');
+$('.phone').mask('+358-0-000-000');
 
 $.fn.setCursorPosition = function(pos) {
-  if ($(this).get(0).setSelectionRange) {
-    $(this).get(0).setSelectionRange(pos, pos);
-  } else if ($(this).get(0).createTextRange) {
-    var range = $(this).get(0).createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', pos);
-    range.moveStart('character', pos);
-    range.select();
-  }
+if ($(this).get(0).setSelectionRange) {
+  $(this).get(0).setSelectionRange(pos, pos);
+} else if ($(this).get(0).createTextRange) {
+  var range = $(this).get(0).createTextRange();
+  range.collapse(true);
+  range.moveEnd('character', pos);
+  range.moveStart('character', pos);
+  range.select();
+}
 };
 
 
-$('input[type="tel"]').click(function(){
-    $(this).setCursorPosition(5);  // set position number
-  });
-</script>
-
-
-
-
-Перед </body> вставить если номер вводить будут Русский
-
-
-      <script src="js/maska-nomera.js" type="text/javascript" ></script>
-
-      <script>
-$('.art-stranger').mask('+7 (999) 999-99-99');
-
-$.fn.setCursorPosition = function(pos) {
-  if ($(this).get(0).setSelectionRange) {
-    $(this).get(0).setSelectionRange(pos, pos);
-  } else if ($(this).get(0).createTextRange) {
-    var range = $(this).get(0).createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', pos);
-    range.moveStart('character', pos);
-    range.select();
-  }
-};
-
-
-$('input[type="tel"]').click(function(){
-    $(this).setCursorPosition(4);  // set position number
-  });
+$('input[type="tel"]').onclick(function(){
+  $(this).setCursorPosition(4);  // set position number
+})
